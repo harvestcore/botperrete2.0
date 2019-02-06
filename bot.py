2fdce275e8b9b386cc2a492a30f50251ef3fd170
harvestcore <aagomezies@gmail.com>
2019-02-07 00:39:39 +0100

    simple bot

---
+++ bot.py
@@ -0,0 +1,8 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from toot import Toot
+
+scheduler = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def job():
+    Toot.commit_tweet("XD")
