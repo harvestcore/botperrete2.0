d28952c5073e2f11581f500eb3d406ac5c34d69f
harvestcore <aagomezies@gmail.com>
2019-02-07 01:18:51 +0100

    cambio tamaño del string a 7

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ sched = BlockingScheduler(timezone='Europe/Madrid')
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    tw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    tw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
     Toot.commit_tweet(tw)
 
 
