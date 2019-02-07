99b1f56d217e972b0fd8b20f58472c91d74fb531
harvestcore <aagomezies@gmail.com>
2019-02-07 01:11:48 +0100

    random string generator

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ sched = BlockingScheduler(timezone='Europe/Madrid')
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    Toot.commit_tweet("XD")
+    tw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    Toot.commit_tweet(tw)
 
 
 sched.start()
