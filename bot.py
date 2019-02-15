b42c1cf83b2a545d5e5a8931525277a765111ae8
harvestcore <aagomezies@gmail.com>
2019-02-15 23:16:36 +0100

    Cambio frecuencia bot

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ import random, string
 
 sched = BlockingScheduler(timezone='Europe/Madrid')
 
-@sched.scheduled_job('interval', minutes=100)
-def timed_job():
-    tw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
-    Toot.commit_tweet(tw)
+# @sched.scheduled_job('interval', minutes=100)
+# def timed_job():
+#     tw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+#     Toot.commit_tweet(tw)
 
-@sched.scheduled_job('interval', minutes=30)
+@sched.scheduled_job('interval', minutes=1)
 def boterino():
     Toot.boterino()
 
